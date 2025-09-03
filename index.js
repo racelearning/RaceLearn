108b24c3f959aaf02e3fdb0265e9f19565458fc9
racelearning <race.elearning@gmail.com>
2025-09-03 21:31:43 +0530

    Add MongoDB connection

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose'); // Add this for future MongoDB use
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
-// Serve React build
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://racelearnuser:g56E2Ha4RnOqpED3@racelearndb.f4hg9bw.mongodb.net/?retryWrites=true&w=majority&appName=RaceLearnDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Serve React build (we’ll add this later)
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-// Handle React routing
+// Handle React routing (we’ll add this later)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
